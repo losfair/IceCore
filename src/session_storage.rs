0e485e8dafdfa12c6a3345a355f19b12385a5dee
losfair <zhy20000919@hotmail.com>
2017-09-02 23:32:28 +0800

    Async session methods

--- src/session_storage.rs
+++ src/session_storage.rs
@@ -1,5 +1,6 @@
 use std::sync::Arc;
 use std::ops::Deref;
+use futures::{future, Future};
 
 pub struct SessionStorage {
     provider: Box<SessionStorageProvider + Send + Sync>
@@ -42,15 +43,30 @@ impl Deref for Session {
 
 pub trait SessionStorageProvider {
     fn create_session(&self) -> Session;
+    fn create_session_async(&self) -> Box<Future<Item = Session, Error = ()>> {
+        Box::new(future::ok(self.create_session()))
+    }
     fn get_session(&self, id: &str) -> Option<Session>;
+    fn get_session_async(&self, id: &str) -> Box<Future<Item = Option<Session>, Error = ()>> {
+        Box::new(future::ok(self.get_session(id)))
+    }
     fn start(&self) {}
 }
 
 pub trait SessionProvider {
     fn get_id(&self) -> String;
     fn get(&self, key: &str) -> Option<String>;
+    fn get_async(&self, key: &str) -> Box<Future<Item = Option<String>, Error = ()>> {
+        Box::new(future::ok(self.get(key)))
+    }
     fn set(&self, key: &str, value: &str);
+    fn set_async(&self, key: &str, value: &str) -> Box<Future<Item = (), Error = ()>> {
+        Box::new(future::ok(self.set(key, value)))
+    }
     fn remove(&self, key: &str) {
         self.set(key, "");
+    }
+    fn remove_async(&self, key: &str) -> Box<Future<Item = (), Error = ()>> {
+        Box::new(future::ok(self.remove(key)))
     }
 }
