2514396a6334a1598f9745a9a7dab5a6a12a6b03
losfair <zhy20000919@hotmail.com>
2017-09-16 00:48:25 +0800

    get_header

--- src/http/request_api.rs
+++ src/http/request_api.rs
@@ -1,4 +1,5 @@
-use std::ffi::CString;
+use std;
+use std::ffi::{CStr, CString};
 use std::os::raw::c_char;
 use hyper;
 
@@ -24,4 +25,23 @@ pub unsafe extern "C" fn ice_http_request_get_remote_addr_to_owned(
         Some(v) => format!("{}", v),
         None => "".to_string()
     }).unwrap().into_raw()
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn ice_http_request_get_header_to_owned(
+    req: &hyper::Request,
+    key: *const c_char
+) -> *mut c_char {
+    let key = CStr::from_ptr(key).to_str().unwrap();
+
+    match req.headers().get_raw(key) {
+        Some(v) => match v.one() {
+            Some(v) => match CString::new(v) {
+                Ok(v) => v.into_raw(),
+                Err(_) => std::ptr::null_mut()
+            },
+            None => std::ptr::null_mut()
+        },
+        None => std::ptr::null_mut()
+    }
 }
