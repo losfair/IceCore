99d88ea0378b65552d65ff7921c2cbcdd77217fa
losfair <zhy20000919@hotmail.com>
2017-09-15 23:16:11 +0800

    Add Server header

--- src/http/server/endpoint_context.rs
+++ src/http/server/endpoint_context.rs
@@ -3,6 +3,7 @@ use std::error::Error;
 use futures;
 use futures::Future;
 use hyper;
+use metadata;
 
 pub struct EndpointContext {
     response_sender: futures::sync::oneshot::Sender<Result<hyper::Response, Box<Error + Send>>>,
@@ -12,6 +13,7 @@ pub struct EndpointContext {
 impl EndpointContext {
     pub fn new_pair(req: Box<hyper::Request>) -> (EndpointContext, Box<Future<Item = hyper::Response, Error = hyper::Error>>) {
         let (resp_tx, resp_rx) = futures::sync::oneshot::channel();
+        let version_string = "Ice/".to_string() + metadata::VERSION;
 
         (EndpointContext {
             response_sender: resp_tx,
@@ -22,9 +24,12 @@ impl EndpointContext {
                     std::io::ErrorKind::Other,
                     e.description()
                 )
-            )).and_then(|r| {
+            )).and_then(move |r| {
                 match r {
-                    Ok(v) => Ok(v),
+                    Ok(mut v) => Ok({
+                        v.headers_mut().set_raw("Server", version_string.as_str());
+                        v
+                    }),
                     Err(e) => Ok(
                         hyper::Response::new().with_status(
                             hyper::StatusCode::InternalServerError
