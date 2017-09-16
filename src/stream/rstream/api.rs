a50bc7663715f286f29298235b87cd9fe33b5afe
losfair <zhy20000919@hotmail.com>
2017-09-16 23:14:25 +0800

    rstream_begin_recv fixes

--- src/stream/rstream/api.rs
+++ src/stream/rstream/api.rs
@@ -8,7 +8,7 @@ use executor;
 #[no_mangle]
 pub extern "C" fn ice_stream_rstream_begin_recv(
     target: &mut ReadStream,
-    cb_on_data: extern fn (call_with: usize, data: *const u8, data_len: u32),
+    cb_on_data: extern fn (call_with: usize, data: *const u8, data_len: u32) -> bool,
     cb_on_end: extern fn (call_with: usize),
     cb_on_error: Option<extern fn (call_with: usize)>,
     call_with: *const c_void
@@ -17,15 +17,19 @@ pub extern "C" fn ice_stream_rstream_begin_recv(
     let receiver = target.take_receiver();
 
     let f = receiver.for_each(move |data| {
-        cb_on_data(call_with, data.as_ptr(), data.len() as u32);
-
-        futures::future::ok(())
+        let result = cb_on_data(call_with, data.as_ptr(), data.len() as u32);
+        if !result {
+            futures::future::err(())
+        } else {
+            futures::future::ok(())
+        }
     })
     .map(move |_| cb_on_end(call_with))
-    .map_err(move |_| {
+    .or_else(move |_| {
         if cb_on_error.is_some() {
             cb_on_error.unwrap()(call_with);
         }
+        Ok(())
     });
 
     executor::get_event_loop().spawn(move |_| f);
